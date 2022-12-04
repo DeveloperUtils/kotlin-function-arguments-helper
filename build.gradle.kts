fa3c010b1b96b5d49add72dd913c18d6eabe26e5
Christoph Graupner <ch.graupner@workingdeveloper.net>
2022-12-04 22:40:20 +0100

    Change compatibility to 2023.1 - update kotlin to 1.7.22 - update org.jetbrains.intellij 1.10.0 - update org.jetbrains.changelog 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.10.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.18.1"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = properties("pluginGroup")
